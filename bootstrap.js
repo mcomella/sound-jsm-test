3b7a64fcd413f263ed9960a09d6522e2112a3fa8
Michael Comella <michael.l.comella@gmail.com>
2014-07-08 17:05:37 -0700

    Add v1 code.

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,6 @@
 const { classes: Cc, interfaces: Ci, utils: Cu } = Components;
 
+// TODO: Cleanup.
 Cu.import("resource://gre/modules/AddonManager.jsm");
 Cu.import("resource://gre/modules/Home.jsm");
 Cu.import("resource://gre/modules/HomeProvider.jsm");
@@ -8,20 +9,11 @@ Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/Task.jsm");
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 
-// The add-on ID defined in install.rdf.
-const ADDON_ID = "ADDON.NAME@SOMEDOMAIN.ORG";
+Cu.import("resource://gre/modules/Sound.jsm");
 
-// Make these IDs unique, preferably tied to a domain that you own.
-const PANEL_ID = "your.panel@somedomain.org";
-const DATASET_ID = "your.dataset@somedomain.org";
+let cr = Cc['@mozilla.org/chrome/chrome-registry;1'].getService(Ci["nsIChromeRegistry"]);
 
-// This file is just an example of what your XHR endpoint could return.
-const DATA_URL = "chrome://youraddon/content/example-items.json";
-
-// An example of how to create a string bundle for localization.
-XPCOMUtils.defineLazyGetter(this, "Strings", function() {
-  return Services.strings.createBundle("chrome://youraddon/locale/youraddon.properties");
-});
+let Log = Cu.import("resource://gre/modules/AndroidLog.jsm", {}).AndroidLog;
 
 // An example of how to import a helper module.
 XPCOMUtils.defineLazyGetter(this, "Helper", function() {
@@ -30,111 +22,27 @@ XPCOMUtils.defineLazyGetter(this, "Helper", function() {
   return sandbox["Helper"];
 });
 
-function optionsCallback() {
-  return {
-    title: Strings.GetStringFromName("title"),
-    views: [{
-      type: Home.panels.View.LIST,
-      dataset: DATASET_ID
-    }],
-    onuninstall: function() {
-      // If your add-on only adds a panel and does nothing else, it is nice to
-      // uninstall the add-on for users if they remove the panel in settings.
-      AddonManager.getAddonByID(ADDON_ID, function(addon) {
-        addon.uninstall();
-      });
-    }
-  };
-}
-
-// An example XHR request to fetch data for panel.
-function fetchData(url, onFinish) {
-  let xhr = Cc["@mozilla.org/xmlextras/xmlhttprequest;1"].createInstance(Ci.nsIXMLHttpRequest);
-  try {
-    xhr.open("GET", url, true);
-  } catch (e) {
-    Cu.reportError("Error opening request to " + url + ": " + e);
-    return;
-  }
-  xhr.onerror = function onerror(e) {
-    Cu.reportError("Error making request to " + url + ": " + e.error);
-  };
-  xhr.onload = function onload(event) {
-    if (xhr.status === 200) {
-      onFinish(xhr.responseText);
-    } else {
-      Cu.reportError("Request to " + url + " returned status " + xhr.status);
-    }
-  };
-  xhr.send(null);
-}
-
-function refreshDataset() {
-  fetchData(DATA_URL, function(response) {
-    Task.spawn(function() {
-      let items = JSON.parse(response);
-      let storage = HomeProvider.getStorage(DATASET_ID);
-      yield storage.deleteAll();
-      yield storage.save(items);
-    }).then(null, e => Cu.reportError("Error refreshing dataset " + DATASET_ID + ": " + e));
-  });
-}
-
-function deleteDataset() {
-  Task.spawn(function() {
-    let storage = HomeProvider.getStorage(DATASET_ID);
-    yield storage.deleteAll();
-  }).then(null, e => Cu.reportError("Error deleting data from HomeProvider: " + e));
-}
-
-// Opens about:home to our new panel.
-function openPanel() {
-  Services.wm.getMostRecentWindow("navigator:browser").BrowserApp.loadURI("about:home?panel=" + PANEL_ID);
-}
-
-/**
- * bootstrap.js API
- * https://developer.mozilla.org/en-US/Add-ons/Bootstrapped_extensions
- */
 function startup(data, reason) {
-  // Always register your panel on startup.
-  Home.panels.register(PANEL_ID, optionsCallback);
-
   switch(reason) {
     case ADDON_INSTALL:
     case ADDON_ENABLE:
-      Home.panels.install(PANEL_ID);
-      HomeProvider.requestSync(DATASET_ID, refreshDataset);
       break;
 
     case ADDON_UPGRADE:
     case ADDON_DOWNGRADE:
-      Home.panels.update(PANEL_ID);
       break;
   }
 
-  // Open the panel when the add-on is first installed.
-  if (reason == ADDON_INSTALL) {
-    openPanel();    
-  }
-
-  // Update data once every hour.
-  // It is okay to call addPeriodicSync every time in order to update the callback.
-  HomeProvider.addPeriodicSync(DATASET_ID, 3600, refreshDataset);
+  let uri = Services.io.newURI('chrome://sound/content/clonepop.wav', null, null);
+  let s = new Sound('chrome://sound/content/clonepop.wav', function () {
+    s.play(function () {
+      s.play();
+      s.play();
+    });
+  });
 }
 
-function shutdown(data, reason) {
-  if (reason == ADDON_UNINSTALL || reason == ADDON_DISABLE) {
-    // Call removePeriodicSync only when uninstalling or disabling,
-    // because we still need periodic sync in other cases.
-    HomeProvider.removePeriodicSync(DATASET_ID);
-
-    Home.panels.uninstall(PANEL_ID);
-    deleteDataset();
-  }
-
-  Home.panels.unregister(PANEL_ID);
-}
+function shutdown(data, reason) {}
 
 function install(data, reason) {}
 
